d4b1a8ad6be31966a709ee3e355d8f1c96bec2a4
yeouchien <yeouchien@gmail.com>
2022-09-27 17:23:08 +0800

    feat: Get assignable members trigger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import 'cross-fetch/polyfill';
+
 import { version as platformVersion } from 'zapier-platform-core';
 
 import { authentication } from './authentication';
@@ -5,6 +7,7 @@ import CreateTask from './creates/create_task';
 import GetAllBlocks from './triggers/get_all_blocks';
 import GetAllProjects from './triggers/get_all_projects';
 import GetAllSpaces from './triggers/get_all_spaces';
+import GetAllAssignableMembers from './triggers/getAllAssignableMembers';
 import TaskDue from './triggers/task_due';
 
 const { version } = require('../package.json');
@@ -18,6 +21,7 @@ export default {
     [GetAllBlocks.key]: GetAllBlocks,
     [GetAllProjects.key]: GetAllProjects,
     [GetAllSpaces.key]: GetAllSpaces,
+    [GetAllAssignableMembers.key]: GetAllAssignableMembers,
     [TaskDue.key]: TaskDue,
   },
 
